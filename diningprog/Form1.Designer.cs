d4ae3a2257dcae2e56e8b52a29400786c889a0a5
zakieki55 <zakueki@gmail.com>
2022-03-31 16:11:52 +0700

    First Commit

--- diningprog/Form1.Designer.cs
+++ diningprog/Form1.Designer.cs
@@ -28,19 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.adventureWorksDataSet = new diningprog.AdventureWorksDataSet();
+            this.employeeBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.employeeTableAdapter = new diningprog.AdventureWorksDataSetTableAdapters.EmployeeTableAdapter();
+            this.employeeIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nationalIDNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contactIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.loginIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.managerIDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.titleDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.birthDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.maritalStatusDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.genderDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hireDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.salariedFlagDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.vacationHoursDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.sickLeaveHoursDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.currentFlagDataGridViewCheckBoxColumn = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.rowguidDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modifiedDateDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorksDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.employeeIDDataGridViewTextBoxColumn,
+            this.nationalIDNumberDataGridViewTextBoxColumn,
+            this.contactIDDataGridViewTextBoxColumn,
+            this.loginIDDataGridViewTextBoxColumn,
+            this.managerIDDataGridViewTextBoxColumn,
+            this.titleDataGridViewTextBoxColumn,
+            this.birthDateDataGridViewTextBoxColumn,
+            this.maritalStatusDataGridViewTextBoxColumn,
+            this.genderDataGridViewTextBoxColumn,
+            this.hireDateDataGridViewTextBoxColumn,
+            this.salariedFlagDataGridViewCheckBoxColumn,
+            this.vacationHoursDataGridViewTextBoxColumn,
+            this.sickLeaveHoursDataGridViewTextBoxColumn,
+            this.currentFlagDataGridViewCheckBoxColumn,
+            this.rowguidDataGridViewTextBoxColumn,
+            this.modifiedDateDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.employeeBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(378, 199);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 0;
+            // 
+            // adventureWorksDataSet
+            // 
+            this.adventureWorksDataSet.DataSetName = "AdventureWorksDataSet";
+            this.adventureWorksDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // employeeBindingSource
+            // 
+            this.employeeBindingSource.DataMember = "Employee";
+            this.employeeBindingSource.DataSource = this.adventureWorksDataSet;
+            // 
+            // employeeTableAdapter
+            // 
+            this.employeeTableAdapter.ClearBeforeFill = true;
+            // 
+            // employeeIDDataGridViewTextBoxColumn
+            // 
+            this.employeeIDDataGridViewTextBoxColumn.DataPropertyName = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.HeaderText = "EmployeeID";
+            this.employeeIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.employeeIDDataGridViewTextBoxColumn.Name = "employeeIDDataGridViewTextBoxColumn";
+            this.employeeIDDataGridViewTextBoxColumn.ReadOnly = true;
+            this.employeeIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // nationalIDNumberDataGridViewTextBoxColumn
+            // 
+            this.nationalIDNumberDataGridViewTextBoxColumn.DataPropertyName = "NationalIDNumber";
+            this.nationalIDNumberDataGridViewTextBoxColumn.HeaderText = "NationalIDNumber";
+            this.nationalIDNumberDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nationalIDNumberDataGridViewTextBoxColumn.Name = "nationalIDNumberDataGridViewTextBoxColumn";
+            this.nationalIDNumberDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // contactIDDataGridViewTextBoxColumn
+            // 
+            this.contactIDDataGridViewTextBoxColumn.DataPropertyName = "ContactID";
+            this.contactIDDataGridViewTextBoxColumn.HeaderText = "ContactID";
+            this.contactIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.contactIDDataGridViewTextBoxColumn.Name = "contactIDDataGridViewTextBoxColumn";
+            this.contactIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // loginIDDataGridViewTextBoxColumn
+            // 
+            this.loginIDDataGridViewTextBoxColumn.DataPropertyName = "LoginID";
+            this.loginIDDataGridViewTextBoxColumn.HeaderText = "LoginID";
+            this.loginIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.loginIDDataGridViewTextBoxColumn.Name = "loginIDDataGridViewTextBoxColumn";
+            this.loginIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // managerIDDataGridViewTextBoxColumn
+            // 
+            this.managerIDDataGridViewTextBoxColumn.DataPropertyName = "ManagerID";
+            this.managerIDDataGridViewTextBoxColumn.HeaderText = "ManagerID";
+            this.managerIDDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.managerIDDataGridViewTextBoxColumn.Name = "managerIDDataGridViewTextBoxColumn";
+            this.managerIDDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // titleDataGridViewTextBoxColumn
+            // 
+            this.titleDataGridViewTextBoxColumn.DataPropertyName = "Title";
+            this.titleDataGridViewTextBoxColumn.HeaderText = "Title";
+            this.titleDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.titleDataGridViewTextBoxColumn.Name = "titleDataGridViewTextBoxColumn";
+            this.titleDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // birthDateDataGridViewTextBoxColumn
+            // 
+            this.birthDateDataGridViewTextBoxColumn.DataPropertyName = "BirthDate";
+            this.birthDateDataGridViewTextBoxColumn.HeaderText = "BirthDate";
+            this.birthDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.birthDateDataGridViewTextBoxColumn.Name = "birthDateDataGridViewTextBoxColumn";
+            this.birthDateDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // maritalStatusDataGridViewTextBoxColumn
+            // 
+            this.maritalStatusDataGridViewTextBoxColumn.DataPropertyName = "MaritalStatus";
+            this.maritalStatusDataGridViewTextBoxColumn.HeaderText = "MaritalStatus";
+            this.maritalStatusDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.maritalStatusDataGridViewTextBoxColumn.Name = "maritalStatusDataGridViewTextBoxColumn";
+            this.maritalStatusDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // genderDataGridViewTextBoxColumn
+            // 
+            this.genderDataGridViewTextBoxColumn.DataPropertyName = "Gender";
+            this.genderDataGridViewTextBoxColumn.HeaderText = "Gender";
+            this.genderDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.genderDataGridViewTextBoxColumn.Name = "genderDataGridViewTextBoxColumn";
+            this.genderDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // hireDateDataGridViewTextBoxColumn
+            // 
+            this.hireDateDataGridViewTextBoxColumn.DataPropertyName = "HireDate";
+            this.hireDateDataGridViewTextBoxColumn.HeaderText = "HireDate";
+            this.hireDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.hireDateDataGridViewTextBoxColumn.Name = "hireDateDataGridViewTextBoxColumn";
+            this.hireDateDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // salariedFlagDataGridViewCheckBoxColumn
+            // 
+            this.salariedFlagDataGridViewCheckBoxColumn.DataPropertyName = "SalariedFlag";
+            this.salariedFlagDataGridViewCheckBoxColumn.HeaderText = "SalariedFlag";
+            this.salariedFlagDataGridViewCheckBoxColumn.MinimumWidth = 6;
+            this.salariedFlagDataGridViewCheckBoxColumn.Name = "salariedFlagDataGridViewCheckBoxColumn";
+            this.salariedFlagDataGridViewCheckBoxColumn.Width = 125;
+            // 
+            // vacationHoursDataGridViewTextBoxColumn
+            // 
+            this.vacationHoursDataGridViewTextBoxColumn.DataPropertyName = "VacationHours";
+            this.vacationHoursDataGridViewTextBoxColumn.HeaderText = "VacationHours";
+            this.vacationHoursDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.vacationHoursDataGridViewTextBoxColumn.Name = "vacationHoursDataGridViewTextBoxColumn";
+            this.vacationHoursDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // sickLeaveHoursDataGridViewTextBoxColumn
+            // 
+            this.sickLeaveHoursDataGridViewTextBoxColumn.DataPropertyName = "SickLeaveHours";
+            this.sickLeaveHoursDataGridViewTextBoxColumn.HeaderText = "SickLeaveHours";
+            this.sickLeaveHoursDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.sickLeaveHoursDataGridViewTextBoxColumn.Name = "sickLeaveHoursDataGridViewTextBoxColumn";
+            this.sickLeaveHoursDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // currentFlagDataGridViewCheckBoxColumn
+            // 
+            this.currentFlagDataGridViewCheckBoxColumn.DataPropertyName = "CurrentFlag";
+            this.currentFlagDataGridViewCheckBoxColumn.HeaderText = "CurrentFlag";
+            this.currentFlagDataGridViewCheckBoxColumn.MinimumWidth = 6;
+            this.currentFlagDataGridViewCheckBoxColumn.Name = "currentFlagDataGridViewCheckBoxColumn";
+            this.currentFlagDataGridViewCheckBoxColumn.Width = 125;
+            // 
+            // rowguidDataGridViewTextBoxColumn
+            // 
+            this.rowguidDataGridViewTextBoxColumn.DataPropertyName = "rowguid";
+            this.rowguidDataGridViewTextBoxColumn.HeaderText = "rowguid";
+            this.rowguidDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.rowguidDataGridViewTextBoxColumn.Name = "rowguidDataGridViewTextBoxColumn";
+            this.rowguidDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // modifiedDateDataGridViewTextBoxColumn
+            // 
+            this.modifiedDateDataGridViewTextBoxColumn.DataPropertyName = "ModifiedDate";
+            this.modifiedDateDataGridViewTextBoxColumn.HeaderText = "ModifiedDate";
+            this.modifiedDateDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.modifiedDateDataGridViewTextBoxColumn.Name = "modifiedDateDataGridViewTextBoxColumn";
+            this.modifiedDateDataGridViewTextBoxColumn.Width = 125;
             // 
             // Form1
             // 
@@ -50,7 +234,10 @@
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "EmployeeDetails";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.adventureWorksDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.employeeBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -58,6 +245,25 @@
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private AdventureWorksDataSet adventureWorksDataSet;
+        private System.Windows.Forms.BindingSource employeeBindingSource;
+        private AdventureWorksDataSetTableAdapters.EmployeeTableAdapter employeeTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn employeeIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nationalIDNumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn contactIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn loginIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn managerIDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn titleDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn birthDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn maritalStatusDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn genderDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hireDateDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn salariedFlagDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn vacationHoursDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn sickLeaveHoursDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn currentFlagDataGridViewCheckBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn rowguidDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modifiedDateDataGridViewTextBoxColumn;
     }
 }
 
